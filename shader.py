5c0ffb9db8248f9deefabc8bbae83f815a73c8e4
Piotr Trybała <trybalapiotr4@gmail.com>
2022-06-12 15:53:00 +0200

    initial commit, rework of PyModelViewer

---
+++ shader.py
@@ -0,0 +1,17 @@
+import os
+
+class Shader:
+    def __init__(self, vertex_file, fragment_file):
+        self.vertex = self.readShader(vertex_file)
+        self.fragment = self.readShader(fragment_file)
+
+    def readShader(self, filename):
+        filepath = f"{os.getcwd()}/shaders/{filename}"
+        try:
+            f = open(filepath, 'r')
+            out = ''.join(f.readlines())
+            f.close()
+            return out
+        except Exception as e:
+            print(f"this file does not exist: {filename}")
+            sys.exit(1)
---
+++ shaders/rectangle.vert
@@ -0,0 +1,12 @@
+#version 330
+
+layout (location = 0) in vec3 a_position;
+
+uniform mat4 model; // combined translation and rotation
+uniform mat4 projection;
+uniform mat4 view;
+
+void main() 
+{
+    gl_Position = projection * view * model * vec4(a_position, 1.0);
+}
